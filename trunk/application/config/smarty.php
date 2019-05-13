3bec5ff9b402ad3569543c285dac28f8cdeb6420
xiaoyan1027 <425373698@qq.com>
2019-05-13 20:03:28 +0800

    调试配置

--- trunk/application/config/smarty.php
+++ trunk/application/config/smarty.php
@@ -3,8 +3,11 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['theme']        = 'default';
 $config['template_dir'] = APPPATH . 'views';
-$config['compile_dir']  = $_SERVER['SINASRV_CACHE_DIR'];
-$config['cache_dir']    = $_SERVER['SINASRV_CACHE_DIR'];
+//$config['compile_dir']  = $_SERVER['SINASRV_CACHE_DIR'];
+//$config['cache_dir']    = $_SERVER['SINASRV_CACHE_DIR'];
+
+$config['compile_dir']  = '/var/log/access_cache';
+$config['cache_dir']    = '/var/log/access_cache';
 $config['config_dir']   = APPPATH . 'configs';
 $config['template_ext'] = '.html';
 $config['caching']      = false;
